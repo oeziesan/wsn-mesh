7ebfcff3eba3621a0cae83c00f7abf56a730295c
Fauzi Abdullah <96937812+oeziesan@users.noreply.github.com>
2025-11-25 15:41:38 +0700

    Update nodeB.ino

--- nodeB/nodeB.ino
+++ nodeB/nodeB.ino
@@ -1,5 +1,5 @@
 // =========================================================
-// ================= CLIENT / RELAY ESP8266 =================
+// ================= nodeB / RELAY ESP8266 =================
 // =========================================================
 #include "painlessMesh.h"
 
@@ -43,7 +43,7 @@ void sendMessage() {
   } else {
     // Broadcast ketika gateway unreachable (biar node lain bisa forward)
     packet = "B|" + String(mesh.getNodeId()) + "|" + payload;
-    mesh.sendBroadcast(packet);
+    mesh.sendSingle(3657532460,packet);
     Serial.printf("[TX] Broadcast (gateway unreachable): %s\n", packet.c_str());
   }
 }
